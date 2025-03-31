10c0167ae06981c3fdacc673af3fd022038a42f6
shai347 <shai347@gmail.com>
2025-03-31 10:57:04 +0700

    Create generate-manifest.js

---
+++ generate-manifest.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const imagesDir = path.join(__dirname, 'public', 'images');
+const manifestPath = path.join(__dirname, 'image-manifest.json');
+
+fs.readdir(imagesDir, (err, files) => {
+  if (err) {
+    console.error('Error reading images directory:', err);
+    return;
+  }
+  // Filter for common image file extensions
+  const images = files.filter(file => /\.(png|jpe?g|gif|svg)$/i.test(file));
+  fs.writeFile(manifestPath, JSON.stringify(images, null, 2), (err) => {
+    if (err) {
+      console.error('Error writing manifest:', err);
+    } else {
+      console.log('Image manifest generated successfully.');
+    }
+  });
+});
